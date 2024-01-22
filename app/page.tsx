d5092db8ee862b00326be9382c0d9722a73afa2d
Evestir <vestir@tuta.io>
2024-01-22 17:26:51 +0900

    mt 20 -> 15

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ import axios from 'axios'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
-      <div className='mt-20'>
+      <div className='mt-[60px]'>
         <PostBoard />
       </div>    
     </main>
